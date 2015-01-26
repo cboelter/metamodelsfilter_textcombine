2d986d0577d8ac1f6513f3ec1b3595ab618d81eb
cboelter <christopher@boelter.eu>
2015-01-26 19:09:18 +0100

    more refactoring

--- contao/config/event_listeners.php
+++ contao/config/event_listeners.php
@@ -18,9 +18,18 @@
 use MetaModels\Filter\Setting\Events\CreateFilterSettingFactoryEvent;
 use MetaModels\Filter\Setting\TextCombineFilterSettingTypeFactory;
 use MetaModels\MetaModelsEvents;
+use MetaModels\DcGeneral\Events\Table\FilterSetting\TextCombine\Subscriber;
+use MetaModels\Events\MetaModelsBootEvent;
+use MetaModels\DcGeneral\Events\Table\FilterSetting\TextCombine\FilterSettingTypeRendererTextCombine;
 
 return array
 (
+    MetaModelsEvents::SUBSYSTEM_BOOT_BACKEND => array(
+        function (MetaModelsBootEvent $event) {
+            new Subscriber($event->getServiceContainer());
+            new FilterSettingTypeRendererTextCombine($event->getServiceContainer());
+        }
+    ),
     MetaModelsEvents::FILTER_SETTING_FACTORY_CREATE => array(
         function (CreateFilterSettingFactoryEvent $event) {
             $event->getFactory()->addTypeFactory(new TextCombineFilterSettingTypeFactory());
