93de673795787a70a8034947e465b1cab71211fc
cboelter <christopher@boelter.eu>
2015-01-26 13:19:57 +0100

    reworked to factories

---
+++ src/MetaModels/Filter/Setting/TextCombineFilterSettingTypeFactory.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * The MetaModels extension allows the creation of multiple collections of custom items,
+ * each with its own unique set of selectable attributes, with attribute extendability.
+ * The Front-End modules allow you to build powerful listing and filtering of the
+ * data in each collection.
+ *
+ * PHP version 5
+ *
+ * @package    MetaModels
+ * @subpackage FilterText
+ * @author     Christopher Boelter <christopher@boelter.eu>
+ * @copyright  The MetaModels team.
+ * @license    LGPL.
+ * @filesource
+ */
+
+namespace MetaModels\Filter\Setting;
+
+/**
+ * Attribute type factory for text filter settings.
+ */
+class TextCombineFilterSettingTypeFactory extends AbstractFilterSettingTypeFactory
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this
+            ->setTypeName('textcombine')
+            ->setTypeIcon('system/modules/metamodelsfilter_textcombine/html/filter_text.png')
+            ->setTypeClass('MetaModels\Filter\Setting\TextCombine')
+            ->allowAttributeTypes();
+
+        foreach (array(
+                     'longtext',
+                     'text',
+                     'translatedtext',
+                     'translatedlongtext',
+                 ) as $attribute) {
+            $this->addKnownAttributeType($attribute);
+        }
+    }
+}
